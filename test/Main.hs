81d577222616f89eb9bf3343285e72dfc4f32a2e
Thomas Scholtes <lambdaqu@gmail.com>
2018-03-04 11:05:22 +0100

    Use resource allocator in tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,42 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
 import Control.Concurrent.STM
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Resource
-
-import           Data.Foldable
-import           Data.Maybe
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-
-import System.Exit
-import System.Environment
-import System.Process
-import Text.Printf
+import Control.Monad.Trans.Resource as Res
 
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.Runners.AntXML
 
 import Test.Setup
+import Test.Support
 
 
 main :: IO ()
-main = runResourceT $ do
-    lastNotification <- setup
-    liftIO $ do
-        defaultMainWithIngredients
-            (antXMLRunner:defaultIngredients)
-            (test_toggleMute lastNotification)
+main = do
+    defaultMainWithIngredients
+        (antXMLRunner:defaultIngredients)
+        tests
+
+tests :: TestTree
+tests = withResource' setup $ \c -> testGroup "cli"
+    [ test_toggleMute c
+    ]
+
+withResource' :: forall a. ResIO a -> (IO a -> TestTree) -> TestTree
+withResource' resource mkTest = withResource acquire close mkTest'
+    where
+        acquire :: IO (a, Res.InternalState)
+        acquire = do
+            resourceState <- Res.createInternalState
+            a <- runInternalState resource resourceState
+            return (a, resourceState)
+
+        close :: (a, Res.InternalState) -> IO ()
+        close (_, resourceState) = closeInternalState resourceState
+
+        mkTest' :: IO (a, Res.InternalState) -> TestTree
+        mkTest' get = mkTest (fst <$> get)
 
 
-
-
-test_toggleMute :: TVar Notification -> TestTree
-test_toggleMute lastNotification = testCaseSteps "toggle mute" $ \step -> do
+test_toggleMute :: IO (TVar Notification) -> TestTree
+test_toggleMute getLastNotification = testCaseSteps "toggle mute" $ \step -> do
+    lastNotification <- getLastNotification
     writePacmdState
         [ "set-default-sink DEFAULT_SINK"
         , "set-sink-volume DEFAULT_SINK 0x8000"
@@ -58,7 +65,7 @@ test_toggleMute lastNotification = testCaseSteps "toggle mute" $ \step -> do
     assertStateLine "set-sink-mute OTHER_SINK yes"
     assertNotificationMessage lastNotification "Volume 50%"
 
-    _ <- readProcess "pacmd" ["set-default-sink", "OTHER_SINK"] []
+    runPacmd ["set-default-sink", "OTHER_SINK"]
 
     step "Other sink unmute"
     runVolumeControl ["mutetoggle"]
@@ -69,52 +76,3 @@ test_toggleMute lastNotification = testCaseSteps "toggle mute" $ \step -> do
     runVolumeControl ["mutetoggle"]
     assertStateLine "set-sink-mute DEFAULT_SINK no"
     assertStateLine "set-sink-mute OTHER_SINK yes"
-
---
--- * Helpers
---
-
-writePacmdState :: [String] -> IO ()
-writePacmdState contents = do
-    filePath <- getEnv "PACMD_STATE_FILE"
-    T.writeFile filePath $ T.pack $ unlines contents
-
-
-assertNotificationMessage :: TVar Notification -> String -> IO ()
-assertNotificationMessage lastNotification expected = do
-    (_, message) <- (atomically $ readTVar lastNotification)
-    message @=? expected
-
-
--- | Assert that the given string is present in the pulseaudio mock
--- state.
-assertStateLine :: String -> IO ()
-assertStateLine line = do
-    let errorTemplate =
-            "Could not find line in pulse audio state\n\
-            \-- EXPECTED -----\n\
-            \%s\n\
-            \-- ACTUAL -------\n\
-            \%s\
-            \-----------------"
-    filePath <- getEnv "PACMD_STATE_FILE"
-    contents <- T.unpack <$> T.readFile filePath
-    let found = find (== line) (lines contents)
-    when (isNothing found) $
-        assertFailure $ printf errorTemplate line contents
-
-
--- | Run 'pa-volume-control' with the given arguments and asserts that
--- the command exits sucesfully.
-runVolumeControl :: [String] -> IO ()
-runVolumeControl args = do
-    let errorTemplate =
-            "pa-volume-control exited with code %d\n\
-            \-- STDERR -------\n\
-            \%s\
-            \-----------------"
-    (exitCode, _stdout, stderr') <- readProcessWithExitCode "pa-volume-control" args []
-    case exitCode of
-        ExitFailure code ->
-            assertFailure $ printf errorTemplate code stderr'
-        _ -> return ()
---
+++ test/Test/Support.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Test.Support (
+    writePacmdState
+  , runVolumeControl
+  , runPacmd
+  , assertNotificationMessage
+  , assertStateLine
+) where
+
+
+import Control.Concurrent.STM
+import Control.Monad
+
+import           Data.Foldable
+import           Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+import System.Exit
+import System.Environment
+import System.Process
+import Text.Printf
+
+import Test.Setup
+import Test.Tasty.HUnit
+
+
+writePacmdState :: [String] -> IO ()
+writePacmdState contents = do
+    filePath <- getEnv "PACMD_STATE_FILE"
+    T.writeFile filePath $ T.pack $ unlines contents
+
+
+assertNotificationMessage :: TVar Notification -> String -> IO ()
+assertNotificationMessage lastNotification expected = do
+    (_, message) <- (atomically $ readTVar lastNotification)
+    message @?= expected
+
+
+-- | Assert that the given string is present in the pulseaudio mock
+-- state.
+assertStateLine :: String -> IO ()
+assertStateLine line = do
+    let errorTemplate =
+            "Could not find line in pulse audio state\n\
+            \-- EXPECTED -----\n\
+            \%s\n\
+            \-- ACTUAL -------\n\
+            \%s\
+            \-----------------"
+    filePath <- getEnv "PACMD_STATE_FILE"
+    contents <- T.unpack <$> T.readFile filePath
+    let found = find (== line) (lines contents)
+    when (isNothing found) $
+        assertFailure $ printf errorTemplate line contents
+
+
+-- | Run 'pa-volume-control' with the given arguments and asserts that
+-- the command exits sucesfully.
+runVolumeControl :: [String] -> IO ()
+runVolumeControl args = do
+    let errorTemplate =
+            "pa-volume-control exited with code %d\n\
+            \-- STDERR -------\n\
+            \%s\
+            \-----------------"
+    (exitCode, _stdout, stderr') <- readProcessWithExitCode "pa-volume-control" args []
+    case exitCode of
+        ExitFailure code ->
+            assertFailure $ printf errorTemplate code stderr'
+        _ -> return ()
+
+runPacmd :: [String] -> IO ()
+runPacmd args = void $ readProcess "pacmd" args []
