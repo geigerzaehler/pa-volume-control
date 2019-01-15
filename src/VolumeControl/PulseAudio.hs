d011368577644827289915f7d7891bc3c9ec6d8c
Thomas Scholtes <lambdaqu@gmail.com>
2019-01-15 18:44:56 +0100

    Use text instead of String

--- src/VolumeControl/PulseAudio.hs
+++ src/VolumeControl/PulseAudio.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
 module VolumeControl.PulseAudio (
     getDefaultSinkState
@@ -10,10 +11,10 @@ module VolumeControl.PulseAudio (
 
 import           Data.Foldable
 import           Data.Int (Int32)
-import           Data.String (lines, words)
 import           Numeric
-import           Text.Read (readMaybe)
 import           System.Process
+import           Text.Read (readMaybe)
+import qualified Data.Text as T
 
 
 -- | Volume as a percentage
@@ -23,25 +24,26 @@ type Volume = Int
 --
 -- Volume control
 --
-setSinkVolume :: String -> Volume -> IO ()
+setSinkVolume :: T.Text -> Volume -> IO ()
 setSinkVolume sink volume = do
     let volume' = max volume 0
     let volString = "0x" <> showHex (volumeToRaw volume') ""
-    _ <- readProcessWithExitCode "pacmd" ["set-sink-volume", sink, volString] ""
+    _ <- readProcessWithExitCode "pacmd" ["set-sink-volume", T.unpack sink, volString] ""
     return ()
 
 
-setSinkMute :: String -> Bool -> IO ()
+setSinkMute :: T.Text -> Bool -> IO ()
 setSinkMute sinkName isMute = do
     let muteString = if isMute then "yes" else "no"
-    _ <- readProcessWithExitCode "pacmd" ["set-sink-mute", sinkName, muteString] ""
+    _ <- readProcessWithExitCode "pacmd" ["set-sink-mute", T.unpack sinkName, muteString] ""
     return ()
 
 
 data SinkState = SinkState
     { _sinkVolume :: Int
     , _sinkIsMuted :: Bool
-    , _sinkName :: String }
+    , _sinkName :: T.Text
+    }
 
 
 -- | Get the sink state for the default sink from the @pacmd@ command.
@@ -57,14 +59,14 @@ getDefaultSinkState = do
         , _sinkIsMuted = sinkIsMuted }
 
 
-readDefaultSink :: PulseAudioDump -> Maybe String
-readDefaultSink = dumpLookup1 "set-default-sink" stringParser
+readDefaultSink :: PulseAudioDump -> Maybe T.Text
+readDefaultSink = dumpLookup1 "set-default-sink" textParser
 
-readSinkMute :: String -> PulseAudioDump -> Maybe Bool
+readSinkMute :: T.Text -> PulseAudioDump -> Maybe Bool
 readSinkMute sinkName =
     dumpLookup2 "set-sink-mute" sinkName boolParser
 
-readSinkVolume :: String -> PulseAudioDump -> Maybe Volume
+readSinkVolume :: T.Text -> PulseAudioDump -> Maybe Volume
 readSinkVolume sinkName =
     dumpLookup2 "set-sink-volume" sinkName volumeParser
 
@@ -72,27 +74,27 @@ readSinkVolume sinkName =
 -- * Dump handling
 --
 
-newtype PulseAudioDump = PulseAudioDump [[String]]
+newtype PulseAudioDump = PulseAudioDump [[T.Text]]
 
 getDump :: IO PulseAudioDump
-getDump = PulseAudioDump . parse <$> readProcess "pacmd" ["dump"] []
+getDump = PulseAudioDump . parse . T.pack <$> readProcess "pacmd" ["dump"] []
   where
-    parse = map words . filter isNotComment . lines
-    isNotComment line = take 1 line /= "#"
+    parse = map T.words . filter isNotComment . T.lines
+    isNotComment line = T.take 1 line /= "#"
 
-dumpLookup1 :: String -> Parser a -> PulseAudioDump -> Maybe a
+dumpLookup1 :: T.Text -> Parser a -> PulseAudioDump -> Maybe a
 dumpLookup1 x1 parse (PulseAudioDump dump) = asum $ map match dump
   where
     match [x1', value] | x1' == x1 = parse value
     match _ = Nothing
 
-dumpLookup2 :: String -> String -> Parser a -> PulseAudioDump -> Maybe a
+dumpLookup2 :: T.Text -> T.Text -> Parser a -> PulseAudioDump -> Maybe a
 dumpLookup2 x1 x2 parse (PulseAudioDump dump) = asum $ map match dump
   where
     match [x1', x2', value] | x1' == x1, x2' == x2 = parse value
     match _ = Nothing
 
-type Parser a = String -> Maybe a
+type Parser a = T.Text -> Maybe a
 
 boolParser :: Parser Bool
 boolParser "yes" = Just True
@@ -100,10 +102,10 @@ boolParser "no" = Just False
 boolParser _ = Nothing
 
 volumeParser :: Parser Volume
-volumeParser value =  volumeFromRaw <$> readMaybe value
+volumeParser value = volumeFromRaw <$> readMaybe (T.unpack value)
 
-stringParser :: Parser String
-stringParser = Just
+textParser :: Parser T.Text
+textParser = Just
 
 
 --
